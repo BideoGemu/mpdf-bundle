4b690309be43f4ef88f60937920f9665e8525173
Jason <71313676+BideoGemu@users.noreply.github.com>
2024-03-14 18:15:34 +0100

    Update BideoGemuMpdfExtension.php for SF6

--- src/DependencyInjection/BideoGemuMpdfExtension.php
+++ src/DependencyInjection/BideoGemuMpdfExtension.php
@@ -1,26 +1,20 @@
 <?php
 namespace BideoGemu\MpdfBundle\DependencyInjection;
 
-use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
-use Symfony\Component\DependencyInjection\Loader;
-use Symfony\Component\HttpKernel\DependencyInjection\Extension;
+use Symfony\Component\DependencyInjection\Extension\Extension;
+use Symfony\Component\Config\FileLocator;
+use Symfony\Component\DependencyInjection\Loader\XmlFileLoader;
+
 
 class BideoGemuMpdfExtension extends Extension
 {
-
-    /**
-     *
-     * {@inheritdoc}
-     *
-     */
-    public function load(array $configs, ContainerBuilder $container)
+    public function load(array $configs, ContainerBuilder $container): void
     {
-
-        $locator = new FileLocator(__DIR__ . '/../Resources/config');
-        $loader = new Loader\YamlFileLoader($container, $locator);
-        $loader->load('services.yaml');
-
+        $loader = new XmlFileLoader(
+            $container,
+            new FileLocator(__DIR__ . '/../Resources/config'')
+        );
+        $loader->load('services.xml');
     }
-
 }
